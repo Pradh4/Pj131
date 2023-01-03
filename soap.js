c8d93d8b4e88a04e50f116c6abc3a3ecc889e012
Pradh4 <103150679+Pradh4@users.noreply.github.com>
2023-01-02 21:01:00 -0800

    Add files via upload

---
+++ soap.js
@@ -0,0 +1,19 @@
+img="";
+
+function setup(){
+canvas = createCanvas(640,480);
+canvas.center();
+}
+
+function preload(){
+    img = loadImage('soap.jpg');
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+    fill('#FF0000');
+    text("Soap",45,75);
+    noFill();
+    stroke('#FF0000');
+    rect(30, 60, 450, 350);
+}
